9882d5049f733131dc7987ce4c21ccba19ece3a4
hebollae <Henri.Bollaert@UGent.be>
2022-09-26 15:44:26 +0200

    added other algorithms

---
+++ algorithms/__init__.py
@@ -0,0 +1,10 @@
+"""
+The Distance Metric Learning module.
+"""
+
+from __future__ import absolute_import
+
+from .nca import NCA
+from .lmnn import LMNN, KLMNN
+from .dmlmj import DMLMJ, KDMLMJ
+from .clnca import ClassNCA
